9f3d3bce0391dc8a2bc26ee68fc6372fd7c7a51f
feykmeelyahoo <feykmeel@yahoo.com>
2017-09-28 14:17:00 +0300

    Added static files

--- first_app/views.py
+++ first_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from first_app.models import Topic, Webpage, AccessRecord
+
 
 # Create your views here.
 
 def index(req):
-    my_dict= {'insert_me':"Hello I am from views.py"}
-    return render(req, 'first_app/index.html', context=my_dict)
+	webpages_list = AccessRecord.objects.order_by("date")
+
+	date_dict = {'access_records': webpages_list}
+
+	my_dict = {'insert_me': "Hello I am from views.py"}
+	return render(req, 'first_app/index.html', context=my_dict)
